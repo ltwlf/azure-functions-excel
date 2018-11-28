b883248e5987313d3524201343a30b7aaeb4c574
Christian Glessner <cg@expertsinside.com>
2018-11-28 23:26:35 +0100

    Write cells was implementd

--- src/api.tests/ExcelServiceTests.cs
+++ src/api.tests/ExcelServiceTests.cs
@@ -2,13 +2,15 @@ using System;
 using Xunit;
 using Ltwlf.Functions.Excel;
 using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace Api.Tests
 {
     public class ExcelServiceTests
     {
         [Fact]
-        public void ReplaceTokens()
+        public void GetCellData()
         {
             var binExcel = File.ReadAllBytes(@"test.xlsx");
             var base64Excel = Convert.ToBase64String(binExcel);
@@ -24,6 +26,33 @@ namespace Api.Tests
                 var actualResult = excelService.GetCellData(template);
 
                 Assert.Equal(expectedResult, actualResult);
+            }
+        }
+
+        [Fact]
+        public void WriteCellData()
+        {
+            var binExcel = File.ReadAllBytes(@"test.xlsx");
+            var base64Excel = Convert.ToBase64String(binExcel);
+
+            var json = @"{
+                ""Tabelle1!B3"":""This is a test too"",
+                ""Tabelle2!A2"": 1234
+            }";
+
+            var data = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+
+            using(var excelService = new ExcelService(base64Excel))
+            {
+                var actualResult = excelService.WriteCellData(data);
+                // not a very goot assert
+                Assert.NotEmpty(actualResult);
+
+                var tempPath = Path.GetTempFileName() + ".xlsx";
+                File.WriteAllBytes(tempPath, Convert.FromBase64String(actualResult));
+
+                // check manually
+                Console.WriteLine(tempPath);
             }
         }
     }
--- src/api/Service/ExcelService.cs
+++ src/api/Service/ExcelService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -41,10 +42,9 @@ public class ExcelService : IDisposable
             var sheetName = token.Groups[1].Value;
             var cellRef = token.Groups[2].Value;
 
-            var sheet = workbookPart.Workbook.Descendants<Sheet>()
-                .Where(s => s.Name == sheetName).FirstOrDefault();
+            var sheet = workbookPart.Workbook.Descendants<Sheet>().Where(s => s.Name == sheetName).FirstOrDefault();
 
-            if(sheet != null)
+            if (sheet != null)
             {
                 var wsPart = (WorksheetPart)(workbookPart.GetPartById(sheet.Id));
 
@@ -55,6 +55,161 @@ public class ExcelService : IDisposable
         }
 
         return template;
+    }
+
+    public string WriteCellData(IDictionary<string, object> data)
+    {
+        var cellRefRegex = new Regex(@"^([a-zA-Z]*)(\d*)$");
+
+        if (sharedStringTable == null)
+        {
+            sharedStringTable = workbookPart.AddNewPart<SharedStringTablePart>();
+        }
+
+        foreach (var key in data.Keys)
+        {
+            var sheetName = key.Split("!")[0];
+            var cellRef = key.Split("!")[1];
+
+            WorksheetPart wsPart = null;
+            var sheet = workbookPart.Workbook.Descendants<Sheet>().Where(s => s.Name == sheetName).FirstOrDefault();
+            if (sheet == null)
+            {
+                wsPart = InsertWorksheet(workbookPart, sheetName);
+            }
+            else
+            {
+                wsPart = (WorksheetPart)(workbookPart.GetPartById(sheet.Id));
+            }
+
+            var cellRefMatch = cellRefRegex.Matches(cellRef).FirstOrDefault();
+            var columnName = cellRefMatch.Groups[1].Value;
+            var rowIndex = (uint)Int32.Parse(cellRefMatch.Groups[2].Value);
+
+            var cell = InsertCellInWorksheet(columnName, rowIndex, wsPart);
+
+            var value = data[key];
+            int strIndex = -1;
+            if (value is string)
+            {
+                strIndex = InsertSharedStringItem(value.ToString());
+                cell.CellValue = new CellValue(strIndex.ToString());
+                cell.DataType = new EnumValue<CellValues>(CellValues.SharedString);
+            }
+            else
+            {
+                cell.CellValue = new CellValue(value.ToString());
+            }
+            wsPart.Worksheet.Save();
+        }
+
+        spreadsheet.WorkbookPart.Workbook.Save();
+        spreadsheet.Save();
+        spreadsheet.Close();
+        spreadsheet.Dispose();
+
+        return Convert.ToBase64String(stream.ToArray());
+
+    }
+
+    private WorksheetPart InsertWorksheet(WorkbookPart workbookPart, string name)
+    {
+        // Add a new worksheet part to the workbook.
+        WorksheetPart newWorksheetPart = workbookPart.AddNewPart<WorksheetPart>();
+        newWorksheetPart.Worksheet = new Worksheet(new SheetData());
+        newWorksheetPart.Worksheet.Save();
+
+        Sheets sheets = workbookPart.Workbook.GetFirstChild<Sheets>();
+        string relationshipId = workbookPart.GetIdOfPart(newWorksheetPart);
+
+        // Get a unique ID for the new sheet.
+        uint sheetId = 1;
+        if (sheets.Elements<Sheet>().Count() > 0)
+        {
+            sheetId = sheets.Elements<Sheet>().Select(s => s.SheetId.Value).Max() + 1;
+        }
+
+        string sheetName = name;
+
+        // Append the new worksheet and associate it with the workbook.
+        Sheet sheet = new Sheet() { Id = relationshipId, SheetId = sheetId, Name = sheetName };
+        sheets.Append(sheet);
+        workbookPart.Workbook.Save();
+
+        return newWorksheetPart;
+    }
+
+
+    private Cell InsertCellInWorksheet(string columnName, uint rowIndex, WorksheetPart worksheetPart)
+    {
+        Worksheet worksheet = worksheetPart.Worksheet;
+        SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+        string cellReference = columnName + rowIndex;
+
+        // If the worksheet does not contain a row with the specified row index, insert one.
+        Row row;
+        if (sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).Count() != 0)
+        {
+            row = sheetData.Elements<Row>().Where(r => r.RowIndex == rowIndex).First();
+        }
+        else
+        {
+            row = new Row() { RowIndex = rowIndex };
+            sheetData.Append(row);
+        }
+
+        // If there is not a cell with the specified column name, insert one.  
+        if (row.Elements<Cell>().Where(c => c.CellReference.Value == columnName + rowIndex).Count() > 0)
+        {
+            return row.Elements<Cell>().Where(c => c.CellReference.Value == cellReference).First();
+        }
+        else
+        {
+            // Cells must be in sequential order according to CellReference. Determine where to insert the new cell.
+            Cell refCell = null;
+            foreach (Cell cell in row.Elements<Cell>())
+            {
+                if (string.Compare(cell.CellReference.Value, cellReference, true) > 0)
+                {
+                    refCell = cell;
+                    break;
+                }
+            }
+
+            Cell newCell = new Cell() { CellReference = cellReference };
+            row.InsertBefore(newCell, refCell);
+
+            worksheet.Save();
+            return newCell;
+        }
+    }
+
+    private int InsertSharedStringItem(string text)
+    {
+        // If the part does not contain a SharedStringTable, create one.
+        if (sharedStringTable.SharedStringTable == null)
+        {
+            sharedStringTable.SharedStringTable = new SharedStringTable();
+        }
+
+        int i = 0;
+
+        // Iterate through all the items in the SharedStringTable. If the text already exists, return its index.
+        foreach (SharedStringItem item in sharedStringTable.SharedStringTable.Elements<SharedStringItem>())
+        {
+            if (item.InnerText == text)
+            {
+                return i;
+            }
+
+            i++;
+        }
+
+        // The text does not exist in the part. Create the SharedStringItem and return its index.
+        sharedStringTable.SharedStringTable.AppendChild(new SharedStringItem(new DocumentFormat.OpenXml.Spreadsheet.Text(text)));
+        sharedStringTable.SharedStringTable.Save();
+
+        return i;
     }
 
     private string GetCellValue(WorksheetPart wsPart, string cellRef)
@@ -113,12 +268,12 @@ public class ExcelService : IDisposable
 
     public void Dispose()
     {
-        if(spreadsheet != null)
+        if (spreadsheet != null)
         {
             spreadsheet.Dispose();
         }
 
-        if(stream != null)
+        if (stream != null)
         {
             stream.Dispose();
         }
