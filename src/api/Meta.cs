0f79330e1a3d1132bd5f4cfb1227e5b44e500a88
Christian Glessner <cg@expertsinside.com>
2018-12-09 17:50:00 +0100

    Meta endpoint was added

---
+++ src/api/Meta.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace api
+{
+    public static class Meta
+    {
+        [FunctionName("meta")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("Meta HTTP trigger function processed a request.");
+
+            var json = await File.ReadAllTextAsync("swagger.json");
+
+            return (ActionResult)new OkObjectResult(json);   
+        }
+    }
+}
